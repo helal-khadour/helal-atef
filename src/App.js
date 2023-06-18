5326c8e22a7de8f081cdbd96b901108c1cc01e96
helal-khadour <helal.khadour@gmail.com>
2023-06-18 16:53:39 +0400

    hero section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Portfolio.png';
 import './App.css';
 
 function App() {
@@ -6,23 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-            <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-          Learn React
-        </a>
+
       </header>
     </div>
   );
